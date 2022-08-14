6cb2ad7816fbf189ce96650193e4bfc985817c35
Carht <carht@protonmail.com>
2022-08-14 19:22:05 -0400

    Testing isMember(c, s string) bool, the length times of s.

--- main_test.go
+++ main_test.go
@@ -18,9 +18,12 @@ func TestGetARandomElementFromAString(t *testing.T) {
 	input := "abcdefghijklmnopqrstuvwxyz"
 	result := getRandomCharacter(input)
 
-	char := isMember(result, input)
+	for range input {
+		char := isMember(result, input)
 
-	if char != true {
-		t.Errorf("The expected value is %t, but the answer was %t", true, char)
+		if char != true {
+			t.Errorf("The expected value is %t, but the answer was %t", true, char)
+		}
 	}
 }
+
