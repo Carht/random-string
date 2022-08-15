d11fc957c2f3350a5fe14f20dd2336f8ba5f24aa
Carht <carht@protonmail.com>
2022-08-15 15:00:54 -0400

    #1 Build binary randomstring example this is a new version of the binary example.

--- examples/main.go
+++ examples/main.go
@@ -8,20 +8,53 @@ import (
 	"github.com/carht/randomstring"
 )
 
+func version() {
+	version := "0.1.1"
+	fmt.Println(version)
+}
+
+func usage() {
+	usage := `randomstring [Argument]
+                  
+                  randomstring:
+                    Without arguments return a string of 32 characters between uppercases, lowercases and numbers.
+                  
+                  randomstring version | randomstring -v:
+                    Return the version of this example.
+
+                  randomstring help | randomstring -h:
+                    Return this help.`
+
+	fmt.Println(usage)
+}
+
+
 func main() {
 	abc := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 	if len(os.Args) > 1 {
-		outStrLen, err := strconv.Atoi(os.Args[1])
-		if err != nil {
-			log.Fatal(err)
+		arg := os.Args[1]
+
+		switch arg {
+		case "version":
+			version()
+		case "-v":
+			version()
+		case "help":
+			usage()
+		case "-h":
+			usage()
+		default:
+			inputLenStr, err := strconv.Atoi(arg)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			randStr := randomstring.GenRandStr(abc, inputLenStr)
+			fmt.Println(randStr)
 		}
-		
-		outStr := randomstring.GenRandStr(abc, outStrLen)
-		fmt.Println(outStr)
-		
 	} else {
-		outStr := randomstring.GenRandStr(abc, 32)
-		fmt.Println(outStr)
-	}	
+		randStr := randomstring.GenRandStr(abc, 32)
+		fmt.Println(randStr)
+	}
 }
