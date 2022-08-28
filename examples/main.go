b3ac95fa87ddb45ffb823d96ad8f1c50b38a41cc
Carht <carht@protonmail.com>
2022-08-28 16:21:24 -0400

    #6 Build differents types of random strings using CLI only random lowercase (and default case) only random uppercase (and default case).
    Default lowercase, uppercase and numbers, the length of this string is 32 characters.

--- examples/main.go
+++ examples/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func version() {
-	version := "0.1.1"
+	version := "0.1.2"
 	fmt.Println(version)
 }
 
@@ -31,15 +31,14 @@ func usage() {
 
 func main() {
 	abc := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	//uABC := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	uABC := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	labc := "abcdefghijklmnopqrstuvwxyz"
 	nums := "0123456789"
 	
 
 	if len(os.Args) > 1 {
 		arg := os.Args[1]
-		arg2 := os.Args[2]
-
+	
 		switch arg {
 		case "version":
 			version()
@@ -50,19 +49,44 @@ func main() {
 		case "-h":
 			usage()
 		case "-n":
-			inputLenStr, err := strconv.Atoi(arg2)
-			if err != nil {
-				log.Fatal(err)
+			if len(os.Args) == 2 {
+				randStr := randomstring.GenRandStr(nums, 32)
+				fmt.Println(randStr)
+			} else {
+				arg2 := os.Args[2]
+				inputLenStr, err := strconv.Atoi(arg2)
+				if err != nil {
+					log.Fatal(err)
+				}
+				randStr := randomstring.GenRandStr(nums, inputLenStr)
+				fmt.Println(randStr)
 			}
-			randStr := randomstring.GenRandStr(nums, inputLenStr)
-			fmt.Println(randStr)
 		case "-l":
-			inputLenStr, err := strconv.Atoi(arg2)
-			if err != nil {
-				log.Fatal(err)
+			if len(os.Args) == 2 {
+				randStr := randomstring.GenRandStr(labc, 32)
+				fmt.Println(randStr)
+			} else {
+				arg2 := os.Args[2]
+				inputLenStr, err := strconv.Atoi(arg2)
+				if err != nil {
+					log.Fatal(err)
+				}
+				randStr := randomstring.GenRandStr(labc, inputLenStr)
+				fmt.Println(randStr)
 			}
-			randStr := randomstring.GenRandStr(labc, inputLenStr)
-			fmt.Println(randStr)
+		case "-u":
+			if len(os.Args) == 2 {
+				randStr := randomstring.GenRandStr(uABC, 32)
+				fmt.Println(randStr)
+			} else {
+				arg2 := os.Args[2]
+				inputLenStr, err := strconv.Atoi(arg2)
+				if err != nil {
+					log.Fatal(err)
+				}
+				randStr := randomstring.GenRandStr(uABC, inputLenStr)
+				fmt.Println(randStr)
+			}
 		default:
 			inputLenStr, err := strconv.Atoi(arg)
 			if err != nil {
